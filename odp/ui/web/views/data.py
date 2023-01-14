15bf129425f5aaec785a6e1e2db547424adce5c1
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2023-01-14 13:27:21 +0200

    Support accessing proxied data servers with an access token

--- odp/ui/web/views/data.py
+++ odp/ui/web/views/data.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, abort, flash, redirect, render_template, url_for
+from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user
 
 from odp.config import config
+from odp.lib.hydra import HydraAdminAPI, OAuth2TokenIntrospection
 
 bp = Blueprint('data', __name__)
+
+hydra_admin_api = HydraAdminAPI(config.HYDRA.ADMIN.URL)
 
 
 @bp.route('/')
@@ -14,15 +17,29 @@ def index():
     )
 
 
-@bp.route('/session')
+@bp.route('/session', methods=('POST',))
 def check_session():
     if not current_user.is_authenticated:
         abort(401)
 
-    return {
-        'subject': current_user.id,
-        'extra': {},
-    }
+    return dict(subject=current_user.id)
+
+
+@bp.route('/token', methods=('POST',))
+def check_token():
+    try:
+        auth_header = request.headers['Authorization']
+        scheme, access_token = auth_header.split()
+        if scheme.lower() != 'bearer':
+            raise ValueError
+    except (KeyError, ValueError):
+        abort(401)
+
+    token: OAuth2TokenIntrospection = hydra_admin_api.introspect_token(access_token)
+    if not token.active:
+        abort(403)
+
+    return dict(subject=token.sub)
 
 
 @bp.route('/error')
