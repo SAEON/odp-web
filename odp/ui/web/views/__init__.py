171bace83b25bd9a7b0ea516ebe7385de1febeab
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2025-01-13 13:31:06 +0200

    Implement SDG tagging in package upload UI

--- odp/ui/web/views/__init__.py
+++ odp/ui/web/views/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 def init_app(app: Flask):
     from . import data, home, proxy
-    from odp.ui.base.views import catalog, packages
+    from odp.ui.base.views import catalog, package, vocabulary
 
     app.register_blueprint(home.bp)
     app.register_blueprint(catalog.bp, url_prefix='/catalog')
+    app.register_blueprint(package.bp, url_prefix='/package')
+    app.register_blueprint(vocabulary.bp, url_prefix='/vocabulary')
     app.register_blueprint(data.bp, url_prefix='/data')
     app.register_blueprint(proxy.bp, url_prefix='/proxy')
-    app.register_blueprint(packages.bp, url_prefix='/upload')
