744a83913bd2b75d799461a2c9a643fc423041e1
dylan <d.pivo@saeon.nrf.ac.za>
2026-02-05 09:45:44 +0200

    submission scopes and nav items

--- odp/ui/web/__init__.py
+++ odp/ui/web/__init__.py
@@ -39,11 +39,13 @@ def create_app():
             HydraScope.OFFLINE_ACCESS,
             ODPScope.RECORD_READ,
             ODPScope.TOKEN_READ,
+            ODPScope.CATALOG_READ,
         ],
         CI_CLIENT_ID=config.ODP.WEB.CI_CLIENT_ID,
         CI_CLIENT_SECRET=config.ODP.WEB.CI_CLIENT_SECRET,
         CI_CLIENT_SCOPE=[
             ODPScope.CATALOG_READ,
+            ODPScope.PROVIDER_READ,
             ODPScope.CATALOG_SEARCH,
             ODPScope.VOCABULARY_READ,
         ],
